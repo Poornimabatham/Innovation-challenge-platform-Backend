feb9d7eaa60781a01153853a8dff7a4ff2f3de21
Poornima Batham <pbatham21@gmail.com>
2026-07-08 21:16:50 +0530

    Feature 1: Authentication + client setup

--- Backend/server/src/middleware/auth.js
+++ Backend/server/src/middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-a
+
 exports.protect = async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token)
